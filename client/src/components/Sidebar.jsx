99ede1a786a9ec37a669adf47a32f427aed2d163
harshavardhan314 <hharshavardhan974@gmail.com>
2025-12-26 22:20:34 +0530

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
-const Sidebar = () => {
+import React, { use } from 'react'
+import assets from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+const Sidebar = ({selectedUser,setSelectedUser}) => {
+  const navigate=useNavigate();
   return (
-    <div>
-        <h1>side</h1>
+    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${
+        selectedUser ? "max-md:hidden" : ""
+      }`}>
+      <div className='pb-5'>
+        <div className='flex justify-between items-center'>
+          <img src={assets.logo} alt="logo" className='max-w-40' />
+            <div className='relative py-2 group'>
+                  <img src={assets.menu_icon} alt="menu" className='max-h-5 cursor-pointer' />
+                    <div className="absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block">
+                      <p onClick={() => navigate('/profile')} className='cursor-pointer text-sm'>Edit profile</p>
+                      <hr  className='my-2 border-t border-gray-500'/>
+                      <p className='cursor-pointer text-sm'>Logout</p>
+                    </div>
+            </div>
+
+        </div>
+
+        <div className=' bg-[#282142] rounded-full flex items-center gap-2 px-4 py-3 mt-5'>
+          <img src={assets.search_icon} alt="search"  className='w-3'/>
+          <input type="text" placeholder='Search here..' className='bg-transparent border-none outline-none text-white  placeholder-[#c8c8c8] flex-1 text-xs'/>
+        </div>
+        
+
+      </div>
       
     </div>
   )
