47229ec6a1ec66e2fcba31f07f957d1ae5c9a6e9
harshavardhan314 <hharshavardhan974@gmail.com>
2026-01-23 22:17:42 +0530

    Update ChatContainer.jsx

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -77,7 +77,8 @@ const ChatContainer = () => {
               </p>
             )}
             <div className="text-center text-xs">
-              <img src={msg.senderId === authUser._id ? authUser.profilePic : selectedUser.profilePic} alt="profile" className="w-7 rounded-full" />
+              <img src={msg.senderId === authUser._id ?authUser?.profilePic || assets.avatar_icon
+                    : selectedUser?.profilePic || assets.avatar_icon} alt="profile" className="w-7 rounded-full" />
               <p className="text-gray-500">{formatMessageTime(msg.createdAt)}</p>
             </div>
           </div>
